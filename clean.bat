f512ef09b89fd04d40979a55da749da52f07fb38
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2017-06-22 15:43:26 -0700

    Add batch file to clean up the directory
    Copied from Chris Grandin's GitHub course

---
+++ clean.bat
@@ -0,0 +1,19 @@
+# Clean up the directory after a LaTeX build. Windows version.
+# Made by Chris Grandin
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lot
+del *.lof
+del *.ps
+del *.toc
+del *.txt
+del *.out
+del *.nav
+del *.snm
+del *.synctex.gz*
+del *.pdf
+del *.concondance.tex
+del *.tex
